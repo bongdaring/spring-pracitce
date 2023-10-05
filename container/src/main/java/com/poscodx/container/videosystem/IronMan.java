a26f345f0313627c69a1c8f4c64269da1f6931ce
bongdaring <chlwlsdud258@gmail.com>
2023-10-05 10:50:50 +0900

    container - Explicit(Xml Config)

---
+++ container/src/main/java/com/poscodx/container/videosystem/IronMan.java
@@ -0,0 +1,14 @@
+package com.poscodx.container.videosystem;
+
+public class IronMan implements DigitalVideoDisc {
+
+	private String title = "Iron Man";
+	private String studio = "MARVEL";
+	
+	@Override
+	public String play() {
+		// TODO Auto-generated method stub
+		return "Playing Movie "+ studio +"'s "+ title;
+	}
+
+}
